3a216280c0b2482fb3080721e8b1ec10a518eb1c
pszeudovirus <egyeddani888@gmail.com>
2022-09-02 18:45:46 +0200

    v0.2 implemented recipe loading

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ struct recipe {
 Material* loadMaterials(int*);
 Recipe* loadRecipes(int*, Material*, const int);
 void AddMaterial(const char*, Material*, int* const, int* const);
-void AddRecipe(char*, Recipe*, const int*, const int*, const Material*, const int);
+void AddRecipe(char*, Recipe*, int* const, int* const, Material* const, const int);
 int IndexOfMaterial(const Material*, const char*, const int);
 
 void AddMaterial(const char* material, Material* materials, int* const len , int* const maxlen)
@@ -59,22 +59,22 @@ Material* loadMaterials(int* const len)
     int maxlen = 10;
 
     char buffer[50];
+    char* material = (char*)malloc(sizeof(char)*50);
 
     while (!feof(materialFile))
     {
         fgets(buffer,50,materialFile);
 
         buffer[strcspn(buffer, "\r\n")] = 0;
-        char* material = (char*)malloc(sizeof(char)*50);
 
         strcpy(material,buffer);    
 
-        AddMaterial(material,materials,len,&maxlen);
+        AddMaterial(material, materials, len, &maxlen);
 
-        free(material);
-
-        printf("Loaded %s\n",buffer);
+        printf("Loaded %s\n", material);
     }
+
+    free(material);
 
     fclose(materialFile);
 
@@ -91,36 +91,95 @@ int IndexOfMaterial(const Material* materials, const char* material, const int l
 }
 
 void AddRecipe(char* recipe, Recipe* recipes,
-    const int* len, const int* maxlen, const Material* materials, const int matLen)
+    int* const len, int* const maxlen,  Material* const materials, const int matLen)
 {
-    if (len == maxlen) {
-        maxlen += 10;
+    if ((*len) == (*maxlen)) {
+        *maxlen += 10;
         recipes = (Recipe*)realloc(recipes,sizeof(Recipe) * (*maxlen));
     }
 
-    char* token;
-    token = strtok_r(recipe,"=>", &recipe);
-    
-    printf("%s\n",token);
+    char* craftMats = strtok_r(recipe, "=>", &recipe);
+    char* result = strtok_r(0, "=>", &recipe);
+
+    char* cmats_tok = strtok_r(craftMats, ",", &craftMats);
+    recipes[*len].materialsLength = 0;
+
+    while (cmats_tok != NULL)
+    {
+        char* amount = strtok_r(cmats_tok,";",&cmats_tok);
+        char* name = strtok_r(0,";",&cmats_tok);
+
+        int index = IndexOfMaterial(materials, name, matLen);
+
+        if (index == -1)
+        {
+            printf("Unkown material: %s\n",name);
+        }
+        else
+        {
+            int craftLen = recipes[*len].materialsLength;
+
+            recipes[*len].materials[craftLen]->amount = atoi(amount);
+            recipes[*len].materials[craftLen]->material = materials + index;
+        }
+
+        cmats_tok = strtok_r(0, ",", &craftMats);
+    }
+
+    char* amount = strtok_r(result,";",&result);
+    char* name = strtok_r(0,";",&result);
+
+    int index = IndexOfMaterial(materials, name, matLen);
+
+    if (index == -1)
+    {
+        printf("Unkown material: %s\n",name);
+    }
+    else
+    {
+        int craftLen = recipes[*len].materialsLength;
+
+        recipes[*len].materials[craftLen]->amount = atoi(amount);
+        recipes[*len].materials[craftLen]->material = materials + index;
+    }
+
+    printf("Reach here\n");
 }
 
 Recipe* loadRecipes(int* len, Material* materials, const int matLen)
 {
     FILE* recipeFile = fopen("recipes.txt","r");
 
+    if (recipeFile == NULL) {
+        printf("Error: File 'materials.txt' couldn't be opened.\n");
+        exit(1);
+    }
+
     Recipe* recipes = (Recipe*)malloc(sizeof(Recipe) * 10);
     int maxlen = 10;
 
     char buffer[500];
+    char* recipe = (char*)malloc(sizeof(char)*500);
 
     while (!feof(recipeFile))
     {
         fgets(buffer,500,recipeFile);
         buffer[strcspn(buffer, "\r\n")] = 0;
-        AddRecipe(buffer,recipes,len,&maxlen,materials,matLen);
 
-        printf("Loaded recipe for %s\n",recipes->result);
+        strcpy(recipe, buffer);
+
+        printf("Recipe: %s\n",recipe);
+
+        AddRecipe(recipe,recipes,len,&maxlen,materials,matLen);
+
+        printf("Loaded: %d %s\n",recipes[(*len)].result->amount,recipes[(*len)].result->material->name);
+
+        printf("Loaded recipe for %s\n",recipes[(*len)].result->material->name);
+
+        (*len)++;
     }
+
+    free(recipe);
 
     fclose(recipeFile);
 
