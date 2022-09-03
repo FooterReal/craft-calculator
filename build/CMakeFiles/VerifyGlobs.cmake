628bab8acafaaa84b37ecd335835bd1207f8e94a
pszeudovirus <egyeddani888@gmail.com>
2022-09-03 08:22:38 +0200

    v0.3 added clean first to cmake command

--- build/CMakeFiles/VerifyGlobs.cmake
+++ build/CMakeFiles/VerifyGlobs.cmake
@@ -3,11 +3,11 @@
 cmake_policy(SET CMP0009 NEW)
 
 # main_SRC at CMakeLists.txt:6 (file)
-file(GLOB NEW_GLOB LIST_DIRECTORIES true "F:/ELTE/Projects/MMC/*.c")
+file(GLOB NEW_GLOB LIST_DIRECTORIES true "F:/ELTE/Projects/MMC/./*.c")
 set(OLD_GLOB
-  "F:/ELTE/Projects/MMC/main.c"
-  "F:/ELTE/Projects/MMC/material.c"
-  "F:/ELTE/Projects/MMC/recipe.c"
+  "F:/ELTE/Projects/MMC/./main.c"
+  "F:/ELTE/Projects/MMC/./material.c"
+  "F:/ELTE/Projects/MMC/./recipe.c"
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
