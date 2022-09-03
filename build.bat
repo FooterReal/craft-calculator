9651f6a9dce97775c18a466c413bfd8a059a33c5
pszeudovirus <egyeddani888@gmail.com>
2022-09-03 09:47:21 +0200

    v0.3 fixed file structure

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Release -S %~dp0 -B %~dp0/build -G "Unix Makefiles"
-cmake . --fresh -G "Unix Makefiles"
+cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Release -S %~dp0/src -B %~dp0/build -G "Unix Makefiles"
+cmake ./src --fresh -G "Unix Makefiles"
 make
