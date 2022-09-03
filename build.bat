77d8ad886489c055856b6eb677c63528887e5847
pszeudovirus <egyeddani888@gmail.com>
2022-09-03 09:32:02 +0200

    v0.3 removing unnecessary files

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
-RMDIR /Q /S ".\build"
 cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Release -S %~dp0 -B %~dp0/build -G "Unix Makefiles"
 cmake . --fresh -G "Unix Makefiles"
 make
