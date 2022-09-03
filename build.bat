e84cac3c85fa4fd04f1a914dcbc9aa82e556e81d
pszeudovirus <egyeddani888@gmail.com>
2022-09-03 08:56:35 +0200

    v0.3 fixed build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
+RMDIR /Q /S ".\build"
+cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_C_COMPILER:FILEPATH=C:\MinGW\bin\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\MinGW\bin\g++.exe -Sf:%~p0 -Bf:%~p0/build -G "Unix Makefiles"
 cmake . --fresh -G "Unix Makefiles"
 make
